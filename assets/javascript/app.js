ff0ea431b6e129f1497010555d56bc0f102f3fdd
Monica Amitin <monzielvsu@gmail.com>
2019-11-03 20:08:21 -0800

    updated firebase link, created text for form in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,21 @@
+
+
+
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCREqhvepbCrJdZ3hWKPQrUOwbsUlCY7XU",
-    authDomain: "train-scheduler-650f9.firebaseapp.com",
-    databaseURL: "https://train-scheduler-650f9.firebaseio.com",
-    projectId: "train-scheduler-650f9",
-    storageBucket: "train-scheduler-650f9.appspot.com",
-    messagingSenderId: "750731861413",
-    appId: "1:750731861413:web:d166cc6732a60c21221c4c",
-    measurementId: "G-DT1N716W84"
-  };
+  apiKey: "AIzaSyBxVsAk9KgE2-Wb7Eu4M4ORUKvmA3QDZAs",
+  authDomain: "train-scheduler-6e4f6.firebaseapp.com",
+  databaseURL: "https://train-scheduler-6e4f6.firebaseio.com",
+  projectId: "train-scheduler-6e4f6",
+  storageBucket: "train-scheduler-6e4f6.appspot.com",
+  messagingSenderId: "783946995749",
+  appId: "1:783946995749:web:4ca47302b83b5ba04147cc",
+  measurementId: "G-0SMHXBWF3Q"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 //Establish connection to database
@@ -24,68 +27,57 @@ var destination = "";
 var frequency = "";
 var arrival = "";
 var minutes = "";
+var firstTrain = "";
 
 //Capure button click
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
   event.preventDefault();
   console.log("test");
 
   //Grab values from text boxes
   name = $("#train-name-input").val().trim();
-  name = $("#destination-input").val().trim();
-  name = $("#frequency-input").val().trim();
+  destination = $("#destination-input").val().trim();
+  frequency = $("#frequency-input").val().trim();
 
   //Code for handling push
   database.ref().push({
     name: name,
     destination: destination,
+    firstTrain: firstTrain,
     frequency: frequency,
-    arrival: arrival,
-    minutes: minutes,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    //dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 });
 
-
-
-
+var count = 0;
 
 // Firebase is always watching for changes to the data.
-    // When changes occurs it will print them to console and html
-    database.ref().on("value", function(snapshot) {
+// When changes occurs it will print them to console and html
+database.ref().on("child_added", function (childSnapshot) {
+  count++
+  console.log("Count: " + count);
 
-        // Print the initial data to the console.
-        console.log(snapshot.val());
+  var frequency = 0;
+  var minutes = 0;
 
-
-
-//moment.js to calculate the time
-  
-        // Log the value of the various properties
-      /*  console.log(snapshot.val().Name);
-        console.log(snapshot.val().Destination);
-        console.log(snapshot.val().Frequency);
-        console.log(snapshot.val().Time);*/
-  
-  
-        // Change the HTML
-        //$("#displayed-data").text(snapshot.val().name + " | " + snapshot.val().age + " | " + snapshot.val().phone);
-  
-        // If any errors are experienced, log them to console.
-      }, function(errorObject) {
-        console.log("The read failed: " + errorObject.code);
-      });
-
-//Create variables
+  // Print the data to the console.
+  console.log(childSnapshot.val().name);
+  console.log(childSnapshot.val().destination);
+  console.log(childSnapshot.val().frequency);
+  console.log(childSnapshot.val().arrival);
+  console.log(childSnapshot.val().minutes);
 
 
+  var tableRow = $("<tr>")
+  tableRow.append($("<td>").text(childSnapshot.val().name));
+  tableRow.append($("<td>").text(childSnapshot.val().destination));
+  tableRow.append($("<td>").text(childSnapshot.val().firstTrain));
+  tableRow.append($("<td>").text(childSnapshot.val().frequency));
+ 
 
+  $("#trainList").append(tableRow);
 
-// Create table for current train schedule
-
-
-
-
-
-
-//create form to add train
+  // If any errors are experienced, log them to console.
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
